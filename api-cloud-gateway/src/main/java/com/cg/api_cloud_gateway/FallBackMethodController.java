8e6b2c209f584160ad3eeb92ce30f243e2b19f82
Bacham-Jagannath <jagannath88860@gmail.com>
2024-09-25 15:27:05 +0530

    api gateway

---
+++ api-cloud-gateway/src/main/java/com/cg/api_cloud_gateway/FallBackMethodController.java
@@ -0,0 +1,17 @@
+package com.cg.api_cloud_gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Service is longer than Expected " + " Please try again";
+        }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department Service is taking longer than Expected " + " Please try again";
+    }
+}
---
+++ api-cloud-gateway/src/test/java/com/cg/api_cloud_gateway/ApiCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.api_cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
